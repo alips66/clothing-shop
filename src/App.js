77aacfe4fbeae0376b83506a2a6d0ed811539dcc
alipu66 <alipu66@gmail.com>
2019-12-07 14:35:47 +0330

    add form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import HomePage from "./pages/homepage/HomePage";
 import {Route, Switch} from 'react-router'
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
 
     return (
         <div className="App">
+            <Header/>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInAndSignUp}/>
             </Switch>
         </div>
     );
