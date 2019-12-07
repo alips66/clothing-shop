fdf3d3ac67354234237ab942c4a43f95b3c69cb4
alipu66 <alipu66@gmail.com>
2019-12-07 09:02:25 +0330

    route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/HomePage";
+import {Route, Switch} from 'react-router'
 
 function App() {
 
     return (
         <div className="App">
-            <HomePage/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/hats' component={HomePage}/>
+            </Switch>
         </div>
     );
 }
